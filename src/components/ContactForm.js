5d2a5b17d521b95b75f55f19b00b3fadbe2ebc4c
Mauro Meden <mauro.meden@gmail.com>
2020-03-26 09:32:17 -0400

    Separate contact form from contact page content.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,62 +2,54 @@ import React from 'react'
 
 function ContactForm() {
     return (
-        <div>
-            <main>
-                <div className="container">
-                    <h2>Get in touch</h2>
-                    <p>I'd love to hear from you! I'll get back to you as soon as I can.</p>
-                    <form action="" target="_self" method="POST">
-                        <div className="form-container">
-                            <div className="row">
-                                <div className="col-narrow">
-                                    <label for="name">Name: </label>
-                                </div>
-                                <div className="col-wide">
-                                    <input type="text" name="name" placeholder="John Smith" required /><br />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-narrow">
-                                    <label for="phone">Phone #: </label>
-                                </div>
-                                <div className="col-wide">
-                                    <input type="tel" name="phone" placeholder="xxx-xxx-xxxx" /><br />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-narrow">
-                                    <label for="email">Email address: </label>
-                                </div>
-                                <div className="col-wide">
-                                    <input type="email" name="email" placeholder="name@example.com" required /><br />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-narrow">
-                                    <label for="subject">Subject: </label>
-                                </div>
-                                <div className="col-wide">
-                                    <input type="text" name="subject" required /><br />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-narrow">
-                                    <label for="message">Your message: </label>
-                                </div>
-                                <div className="col-wide">
-                                    <textarea rows="10" cols="30" name="message" required></textarea><br />
-                                </div>
-                            </div>
-                            <div className="row col-narrow">
-                                <input type="submit" value="Send message" />
-                                <input type="reset" value="Reset" />
-                            </div>
-                        </div>
-                    </form>
+        <form action="" target="_self" method="POST">
+            <div className="form-container">
+                <div className="row">
+                    <div className="col-narrow">
+                        <label for="name">Name: </label>
+                    </div>
+                    <div className="col-wide">
+                        <input type="text" name="name" placeholder="John Smith" required /><br />
+                    </div>
                 </div>
-            </main>
-        </div>
+                <div className="row">
+                    <div className="col-narrow">
+                        <label for="phone">Phone #: </label>
+                    </div>
+                    <div className="col-wide">
+                        <input type="tel" name="phone" placeholder="xxx-xxx-xxxx" /><br />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-narrow">
+                        <label for="email">Email address: </label>
+                    </div>
+                    <div className="col-wide">
+                        <input type="email" name="email" placeholder="name@example.com" required /><br />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-narrow">
+                        <label for="subject">Subject: </label>
+                    </div>
+                    <div className="col-wide">
+                        <input type="text" name="subject" required /><br />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-narrow">
+                        <label for="message">Your message: </label>
+                    </div>
+                    <div className="col-wide">
+                        <textarea rows="10" cols="30" name="message" required></textarea><br />
+                    </div>
+                </div>
+                <div className="row col-narrow">
+                    <input type="submit" value="Send message" />
+                    <input type="reset" value="Reset" />
+                </div>
+            </div>
+        </form>
     )
 }
 
--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -4,9 +4,16 @@ import ContactForm from '../ContactForm'
 
 function ContactMe() {
     return (
-        <div>
-            <ContactForm />
-        </div>
+        <main>
+            <div className="container">
+                <h2>Get in touch</h2>
+                <p>
+                    I'd love to hear from you! <br />
+                    I'll get back to you as soon as I can.
+                </p>
+                <ContactForm />
+            </div>
+        </main>
     )
 }
 
