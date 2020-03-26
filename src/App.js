bbcd164712014eec31e2984aff4b0537c30cd34e
Mauro Meden <mauro.meden@gmail.com>
2020-03-26 07:44:43 -0400

    Add Home and About page components and routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
+
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
