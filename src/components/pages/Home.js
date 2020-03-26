0308629ca6b76e0c897ad5c50d360ce3226a8562
Mauro Meden <mauro.meden@gmail.com>
2020-03-26 07:52:58 -0400

    Add Home page content.
    Add images folder.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,35 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>Home page</h1>
+            <main>
+                <div class="container">
+                    <p>
+                        "We come from Nature. We are connected to it throughout our whole lives, there is an importance to it. If we
+        destroy nature, we will ultimately destroy ourselves"<br />
+                        <span class="quote">- Michael</span>
+                    </p>
+                </div>
+            </main>
+
+            <aside>
+                <div class="sidebar services">
+                    <h2>Services</h2>
+                    <p>
+                        Book a lesson<br />
+                        Book a photoshoot
+                    </p>
+                    <p><a href="contactus.html">Contact Michael</a></p>
+                </div>
+            </aside>
+
+            <aside>
+                <div class="recentwork">
+                    <h2>Recent Work</h2>
+                    <img src="images/lakes/lakes_22.jpg" width="100%" alt="Lake" />
+                    <img src="images/orest/forest_22.jpg" width="100%" alt="Forest" />
+                    <img src="images/nightSky/nightsky_22.jpg" width="100%" alt="Night Sky" />
+                </div>
+            </aside>
         </div>
     )
 }
