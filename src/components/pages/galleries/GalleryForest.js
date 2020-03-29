84bad302938c4a82144d4862475b18c608820f88
maurom88 <mauro.meden@gmail.com>
2020-03-29 10:23:35 -0400

    Style forest gallery page.

--- src/components/pages/galleries/GalleryForest.js
+++ src/components/pages/galleries/GalleryForest.js
@@ -4,12 +4,20 @@ import GalleryPic from '../../items/GalleryPic'
 
 function GalleryForest() {
     return (
-        <div>
-            <main>
-                <GalleryPic height="400" width="600" href="#" desc="Forest 1" alt="Forest 1" src="../images/forests/forest_1.jpg" />
-                <GalleryPic height="400" width="600" href="#" desc="Forest 2" alt="Forest 2" src="../images/forests/forest_2.jpg" />
-            </main>
-        </div>
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <GalleryPic divClassName="gallery-pic m-2" imgClassName="img-thumbnail"  href="#"
+                        desc="Forest 1" alt="Forest 1" src="../images/forests/forest_1.jpg" />
+                    </div>
+                    <div className="col-sm-6">
+                        <GalleryPic  divClassName="gallery-pic m-2" imgClassName="img-thumbnail"  href="#"
+                            desc="Forest 2" alt="Forest 2" src="../images/forests/forest_2.jpg" />
+                    </div>
+                </div>
+            </div>
+        </main >
     )
 }
 
