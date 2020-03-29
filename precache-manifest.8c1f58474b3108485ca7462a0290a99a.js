d2f53dd33a791cf3c2e15124a4aa4548756c278f
maurom88 <mauro.meden@gmail.com>
2020-03-29 13:09:09 -0400

    Updates

---
+++ precache-manifest.8c1f58474b3108485ca7462a0290a99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e13762c473902c9543371da116639",
+    "url": "/new-michael-landscapes/index.html"
+  },
+  {
+    "revision": "a3566be73859c34d05c6",
+    "url": "/new-michael-landscapes/static/css/main.9cca3c41.chunk.css"
+  },
+  {
+    "revision": "26c14bc4ba932ce5f27d",
+    "url": "/new-michael-landscapes/static/js/2.451f65e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-michael-landscapes/static/js/2.451f65e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3566be73859c34d05c6",
+    "url": "/new-michael-landscapes/static/js/main.352bbbb0.chunk.js"
+  },
+  {
+    "revision": "4ab989148a3684859c6a",
+    "url": "/new-michael-landscapes/static/js/runtime-main.025613d1.js"
+  },
+  {
+    "revision": "98f41b707d2ab4f25fb26c5b91a49268",
+    "url": "/new-michael-landscapes/static/media/header.98f41b70.jpg"
+  }
+]);
