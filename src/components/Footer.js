99b7eb290e96144f57c5ebd36ba1f72ebbbb6d1e
maurom88 <mauro.meden@gmail.com>
2020-03-30 07:26:06 -0400

    Add pics credits in footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,13 @@ function Footer() {
                     <br />
                     Code by <a href="http://www.mauromeden.com">Mauro Meden</a>
                 </p>
+                <p>
+                    Pictures courtesy of:&nbsp;
+                    <a href="https://www.pexels.com/">Pexels</a> -&nbsp;
+                    <a href="https://www.freepik.com/free-photos-vectors/">Freepik</a> -&nbsp;
+                    <a href="https://pixabay.com/">Pixabay</a>
+                </p>
+
             </div>
         </footer>
     )
